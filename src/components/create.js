12aa5dc7f62107aeb1406adf11d40911404c11e0
Lucky Masuku <g00363602@gmit.ie>
2020-12-30 01:14:57 +0000

    adding forms

--- src/components/create.js
+++ src/components/create.js
@@ -2,12 +2,89 @@ import React from 'react';
 
 
 //Class extends from react
-export class Create extends React.Component{
-    render(){
-        return(
-            <div>
-                <h1>This is the create Component.</h1>
-                </div>
+export class Create extends React.Component {
+
+    //
+    constructor() {
+        super();
+
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChangePlayer = this.onChangePlayer.bind(this);
+        this.onChangeVenue = this.onChangeVenue.bind(this);
+        this.onChangeTeam = this.onChangeTeam.bind(this);
+
+        this.state = {
+            Player: '',
+            Venue: '',
+            Team: ''
+
+        }
+    }
+    //when  the value changes update the state
+    onChangePlayer(e) {
+        this.setState({
+            Player: e.target.value
+        });
+    }
+    // update the value when the state changes
+    onChangeVenue(e) {
+        this.setState({
+            Venue: e.target.value
+        });
+    }
+    // update the value when the state changes
+    onChangeTeam(e) {
+        this.setState({
+            Team: e.target.value
+
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        alert("Match: " + this.state.Player + " " + this.state.Venue + " " + this.state.Team);
+    }
+    render() {
+        return (
+            <div className='App'>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Add Match Player: </label>
+                        <input type='text'
+                            className='form-control'
+                            value={this.state.Player}
+                            onChange={this.onChangePlayer}></input>
+
+                    </div>
+
+                    <div className="form-group">
+                        <label>Add Match Venue:</label>
+                        <input type='text'
+                            className='form-control'
+                            value={this.state.Venue}
+                            onChange={this.onChangeVenue}>
+                        </input>
+                    </div>
+
+                    <div className="form-group">
+                        <label>Match Team:</label>
+                        <textarea type='text'
+                            className='form-control'
+                            value={this.state.Team}
+                            onChange={this.onChangeTeam}>
+                        </textarea>
+                    </div>
+
+                    <div className="form-group">
+                        <input type='submit'
+                            value='Add Match'
+                            className='btn btn-primary'>
+
+                        </input>
+                    </div>
+                </form>
+
+            </div>
         )
     }
 
